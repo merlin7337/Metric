2f98bc3fee61c52cad23efbc27a1dab07c32283e
Fatum73 <112899660+Fatum73@users.noreply.github.com>
2023-04-16 21:05:06 +0300

    Refactored upcoming view with loops

--- src/Pages/Upcoming/Upcoming.jsx
+++ src/Pages/Upcoming/Upcoming.jsx
@@ -8,81 +8,38 @@ export default function Upcoming() {
     moment().add(i, "days").format("DD.MM.YYYY")
   );
 
+  const upcomingWeekTaskLists = Array.from({ length: 7 }).map((_, i) => {
+    return i === 0 || i > 6 ? (
+      <div className={cl.taskListContainer}>
+        <div className={cl.dateHeading}>
+          {moment(upcomingWeek[i], "DD.MM.YYYY").format("DD MMM")}
+        </div>
+        <TaskList
+          filter={(e) => e.dueDate === upcomingWeek[i]}
+          defaultValue={{ dueDate: upcomingWeek[i] }}
+        />
+      </div>
+    ) : (
+      <>
+        <div className={cl.verticalDivider} />
+        <div className={cl.taskListContainer}>
+          <div className={cl.dateHeading}>
+            {moment(upcomingWeek[i], "DD.MM.YYYY").format("DD MMM")}
+          </div>
+          <TaskList
+            filter={(e) => e.dueDate === upcomingWeek[i]}
+            defaultValue={{ dueDate: upcomingWeek[i] }}
+          />
+        </div>
+      </>
+    );
+  });
+
   return (
     <div className={cl.container}>
       <div className={cl.content}>
         <h2 className={cl.heading}>Upcoming</h2>
-        <div className={cl.upcomingContainer}>
-          <div className={cl.taskListContainer}>
-            <div className={[cl.dateHeading, cl.today].join(" ")}>
-              {moment(upcomingWeek[0], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[0]}
-              defaultValue={{ dueDate: upcomingWeek[0] }}
-            />
-          </div>
-          <div className={cl.verticalDivider} />
-          <div className={cl.taskListContainer}>
-            <div className={cl.dateHeading}>
-              {moment(upcomingWeek[1], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[1]}
-              defaultValue={{ dueDate: upcomingWeek[1] }}
-            />
-          </div>
-          <div className={cl.verticalDivider} />
-          <div className={cl.taskListContainer}>
-            <div className={cl.dateHeading}>
-              {moment(upcomingWeek[2], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[2]}
-              defaultValue={{ dueDate: upcomingWeek[2] }}
-            />
-          </div>
-          <div className={cl.verticalDivider} />
-          <div className={cl.taskListContainer}>
-            <div className={cl.dateHeading}>
-              {moment(upcomingWeek[3], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[3]}
-              defaultValue={{ dueDate: upcomingWeek[3] }}
-            />
-          </div>
-          <div className={cl.verticalDivider} />
-          <div className={cl.taskListContainer}>
-            <div className={cl.dateHeading}>
-              {moment(upcomingWeek[4], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[4]}
-              defaultValue={{ dueDate: upcomingWeek[4] }}
-            />
-          </div>
-          <div className={cl.verticalDivider} />
-          <div className={cl.taskListContainer}>
-            <div className={cl.dateHeading}>
-              {moment(upcomingWeek[5], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[5]}
-              defaultValue={{ dueDate: upcomingWeek[5] }}
-            />
-          </div>
-          <div className={cl.verticalDivider} />
-          <div className={cl.taskListContainer}>
-            <div className={cl.dateHeading}>
-              {moment(upcomingWeek[6], "DD.MM.YYYY").format("DD MMM")}
-            </div>
-            <TaskList
-              filter={(e) => e.dueDate === upcomingWeek[6]}
-              defaultValue={{ dueDate: upcomingWeek[6] }}
-            />
-          </div>
-        </div>
+        <div className={cl.upcomingContainer}>{upcomingWeekTaskLists}</div>
       </div>
     </div>
   );
